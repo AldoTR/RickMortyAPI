208fd7b43cc07c9d1e518dbca87443491970bc24
zS20006781@estudiantes.uv.mx <zS20006781@estudiantes.uv.mx>
2023-03-24 11:46:27 -0600

    App edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aldo Torres Ramírez
+          Aldo Torres Ramírez aplication  
         </p>
         <a
           className="App-link"
